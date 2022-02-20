9b2fef1043a155685a0b9dc1f6aee4d3b89c6cb8
Hanpiranpm <annromania@gmail.com>
2022-02-20 22:24:20 +0300

    feat: add parametres for empty array

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (arguments.length == 0) {
+        return [];
+    } else if (Array.isArray(matrix)) {
+        if (matrix.length == 0) {
+            return [];
+        }
+    } else {
+        
+    }
 }
