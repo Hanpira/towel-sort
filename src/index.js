a9f9c29b1e57652a9384fff712673b8a901589e3
Hanpiranpm <annromania@gmail.com>
2022-02-24 01:03:30 +0300

    feat: add final version

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@
 module.exports = function towelSort (matrix) {
     if (arguments.length == 0) {
         return [];
-    } else if (Array.isArray(matrix)) {
-        if (matrix.length == 0) {
-            return [];
-        }
+    } else if (Array.isArray(matrix) && matrix.length == 0) {
+        return [];
     } else {
-        
+        let newMatrix = [];
+        for (let i = 0; i < matrix.length; i++) {
+            let arr = matrix[i];
+            if ((i + 1) % 2 == 0) {   // we need to reverse only odd 
+                arr = arr.reverse(); 
+            }
+            newMatrix = newMatrix.concat(arr);
+        }
+        return newMatrix;
     }
 }
